7e8f109a2a4e79d7e0816190f1534eb4fcca9bd3
Gnome <david2005thomas@gmail.com>
2023-04-11 16:07:00 +0000

    Remove headers for SaveManager.h

--- payload/game/system/SaveManager.h
+++ payload/game/system/SaveManager.h
@@ -3,9 +3,3 @@
 #include "game/system/Mii.h"
 
 bool SaveManager_SaveGhostResult(void);
-
-u32 SaveManager_GetVanillaMode(void);
-
-u32 SaveManager_GetFOV169(void);
-
-u32 SaveManager_GetMapIcons(void);
