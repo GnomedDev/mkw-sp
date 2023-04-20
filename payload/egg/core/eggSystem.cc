a8e53666614392244bea531a88de50aa986710e7
GnomedDev <david2005thomas@gmail.com>
2023-04-20 10:54:41 +0100

    WIP

--- payload/egg/core/eggSystem.cc
+++ payload/egg/core/eggSystem.cc
@@ -1,5 +1,12 @@
 #include "eggSystem.hh"
 
+extern "C" {
+#include <game/system/Console.h>
+#include <sp/Commands.h>
+#include <sp/ItemCommand.h>
+#include <sp/keyboard/Keyboard.h>
+}
+
 namespace EGG {
 
 void *TSystem::mem1ArenaLo() const {
@@ -18,12 +25,29 @@ void *TSystem::mem2ArenaHi() const {
     return m_mem2ArenaHi;
 }
 
-XfbManager *TSystem::xfbManager() {
-    return m_xfbManager;
-}
-
 TSystem *TSystem::Instance() {
     return &s_instance;
+}
+
+void TSystem::onBeginFrame() {
+    Item_beginFrame();
+    if (m_consoleInputsUnavailable) {
+        return;
+    }
+
+    if (!SP_IsConsoleInputInit()) {
+        if (SP_InitConsoleInput()) {
+            Commands_init();
+            SP_SetLineCallback(Commands_lineCallback);
+        } else {
+            // Do not try again
+            m_consoleInputsUnavailable = true;
+            return;
+        }
+    }
+
+    SP_ProcessConsoleInput();
+    Console_calc();
 }
 
 } // namespace EGG
--- payload/egg/core/eggSystem.hh
+++ payload/egg/core/eggSystem.hh
@@ -6,25 +6,41 @@ namespace EGG {
 
 class TSystem {
 public:
+    TSystem();
+    virtual ~TSystem();
+    virtual void *dt(s32 type);
+
+    virtual void *getVideo();
+    virtual void *getSysHeap();
+    virtual void *getDisplay();
+    virtual XfbManager* getXfbManager();
+    virtual void *getPerformanceView();
+    virtual void *getSceneManager();
+    virtual void *getAudioManager();
+    REPLACE virtual void onBeginFrame();
+
     void *mem1ArenaLo() const;
     void *mem1ArenaHi() const;
     void *mem2ArenaLo() const;
     void *mem2ArenaHi() const;
-    XfbManager *xfbManager();
 
     static TSystem *Instance();
 
 private:
-    u8 _00[0x04 - 0x00];
     void *m_mem1ArenaLo;
     void *m_mem1ArenaHi;
     void *m_mem2ArenaLo;
     void *m_mem2ArenaHi;
     u8 _14[0x48 - 0x14];
     XfbManager *m_xfbManager;
-    // ...
+    u8 _4c[0x68 - 0x4c];
+    u8 m_frameClock;
+    u8 m_consoleInputsUnavailable; // Was padding
+    u8 _6a[0x74 - 0x6a];
 
     static TSystem s_instance;
 };
+
+static_assert(sizeof(TSystem) == 0x74);
 
 } // namespace EGG
