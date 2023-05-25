cf3357c8543da4522fc31c7c00cd2a746267b21e
Gnome <david2005thomas@gmail.com>
2023-05-25 22:50:38 +0100

    WIP

--- tools/ghost-server/src/main.rs
+++ tools/ghost-server/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Instant};
 
 use anyhow::{Context, Result};
 use libhydrogen::{kx, secretbox};
@@ -8,19 +8,35 @@ use netprotocol::{
 };
 use parking_lot::RwLock;
 
-mod leaderboard;
+const TIME_TO_LIVE: u64 = 60 * 60;
+type RegionalLeaderboard = [tokio::sync::RwLock<Option<Leaderboard>>; 32];
+
+#[derive(Debug)]
+struct Leaderboard {
+    response: GhostResponse,
+    time_created: Instant,
+}
+
+impl Default for Leaderboard {
+    fn default() -> Self {
+        Self {
+            response: GhostResponse::default(),
+            time_created: Instant::now(),
+        }
+    }
+}
 
 #[derive(Debug, Default)]
 struct Cache {
-    world: [GhostResponse; 32],
-    americas: [GhostResponse; 32],
-    europe: [GhostResponse; 32],
-    asia: [GhostResponse; 32],
-    oceania: [GhostResponse; 32],
+    world: RegionalLeaderboard,
+    americas: RegionalLeaderboard,
+    europe: RegionalLeaderboard,
+    asia: RegionalLeaderboard,
+    oceania: RegionalLeaderboard,
 }
 
 impl Cache {
-    fn leaderboards(&self, region: &str) -> Option<&[GhostResponse; 32]> {
+    fn leaderboards(&self, region: &str) -> Option<&RegionalLeaderboard> {
         Some(match region {
             "world" => &self.world,
             "americas" => &self.americas,
@@ -30,20 +46,10 @@ impl Cache {
             _ => return None,
         })
     }
-    fn leaderboards_mut(&mut self, region: &str) -> Option<&mut [GhostResponse; 32]> {
-        Some(match region {
-            "world" => &mut self.world,
-            "americas" => &mut self.americas,
-            "europe" => &mut self.europe,
-            "asia" => &mut self.asia,
-            "oceania" => &mut self.oceania,
-            _ => return None,
-        })
-    }
 }
 
 async fn fetch_leaderboard(
-    cache: Arc<RwLock<Cache>>,
+    existing_leaderboard: &mut Option<Leaderboard>,
     session: &reqwest::Client,
     region: &str,
     track: usize,
@@ -73,43 +79,55 @@ async fn fetch_leaderboard(
         millis: r.score % 1000,
     }));
 
-    cache.write().leaderboards_mut(region).unwrap()[track - 49] = response;
+    *existing_leaderboard = Some(Leaderboard {
+        response,
+        time_created: Instant::now(),
+    });
+
     Ok(())
-}
-
-async fn fetch_leaderboards(session: &reqwest::Client) -> Result<Arc<RwLock<Cache>>> {
-    let mut tasks = Vec::new();
-    let cache = Arc::new(RwLock::new(Cache::default()));
-
-    for region in ["world", "americas", "europe", "asia", "oceania"] {
-        for track in 49..81 {
-            let fut = fetch_leaderboard(cache.clone(), session, region, track);
-            tasks.push(fut)
-        }
-    }
-
-    println!("{}", tasks.len());
-    futures::future::try_join_all(tasks).await?;
-    Ok(dbg!(cache))
 }
 
 async fn handle_client(
     stream: tokio::net::TcpStream,
     server_keypair: kx::KeyPair,
-    cache: Arc<RwLock<Cache>>,
+    client: reqwest::Client,
+    cache: Arc<Cache>,
 ) -> Result<()> {
     let context = secretbox::Context::from(*b"ghost   ");
     let mut connection =
-        AsyncStream::<GhostRequest, _>::new(stream, server_keypair, context).await?;
+        AsyncStream::<GhostRequest, GhostResponse>::new(stream, server_keypair, context).await?;
 
     loop {
-        let Some(request) = connection.read().await? else {return Ok(())};
-        let response = match cache
-            .read()
-            .leaderboards(&request.region)
-            .and_then(|l| l.get(request.track as usize))
-        {
-            Some(leaderboard) => leaderboard.clone(),
+        let Some(GhostRequest {region, track}) = connection.read().await? else {return Ok(())};
+        let track = track as usize;
+
+        let response = match cache.leaderboards(&region).and_then(|l| l.get(track)) {
+            Some(leaderboard_lock) => {
+                let leaderboard_read = leaderboard_lock.read().await;
+                if let Some(leaderboard) = &*leaderboard_read {
+                    let time_created = leaderboard.time_created;
+                    if time_created.elapsed().as_secs() > TIME_TO_LIVE {
+                        drop(leaderboard_read);
+                        let mut leaderboard_write = leaderboard_lock.write().await;
+                        if leaderboard_write.as_mut().unwrap().time_created == time_created {
+                            fetch_leaderboard(&mut leaderboard_write, &client, &region, track)
+                                .await?;
+                        }
+
+                        leaderboard_write.as_mut().unwrap().response.clone()
+                    } else {
+                        leaderboard.response.clone()
+                    }
+                } else {
+                    drop(leaderboard_read);
+                    let mut leaderboard_write = leaderboard_lock.write().await;
+                    if leaderboard_write.is_none() {
+                        fetch_leaderboard(&mut leaderboard_write, &client, &region, track).await?;
+                    }
+
+                    leaderboard_write.as_mut().unwrap().response.clone()
+                }
+            }
             None => continue,
         };
 
@@ -123,7 +141,7 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let session = reqwest::Client::new();
-    let cache = fetch_leaderboards(&session).await?;
+    let cache = Arc::new(Cache::default());
 
     let server_keypair = kx::KeyPair::gen();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:21333").await?;
@@ -141,10 +159,11 @@ async fn main() -> Result<()> {
         };
 
         let cache = cache.clone();
+        let session = session.clone();
         let server_keypair = server_keypair.clone();
 
         tokio::spawn(async move {
-            match handle_client(stream, server_keypair, cache).await {
+            match handle_client(stream, server_keypair, session, cache).await {
                 Ok(()) => {
                     tracing::info!("{peer_addr}: Successfully initialized client");
                 }
